40363d68d983e00327fa75c42913a67420bc5f13
MohdSalsabeel <103497056+MohdSalsabeel@users.noreply.github.com>
2022-04-28 17:50:37 +0530

    Create User_module.h

---
+++ User_module.h
@@ -0,0 +1,26 @@
+
+#ifndef __User_module_h
+#define __User_module_h
+
+#include<string>
+using namespace std;
+
+// We have declared class for User
+
+class User
+{
+   public:
+    string user_name;
+    string email_id;
+    string user_id;
+    string password;
+    string pin;
+    string imei_no;
+    string location;
+    unsigned long long int  mobile_no_int;
+    string mobile_no;
+    string time;
+
+};
+
+#endif
