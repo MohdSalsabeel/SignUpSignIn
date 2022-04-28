b5b463de071fd2a42e840e7847dfe3361cace3f6
MohdSalsabeel <103497056+MohdSalsabeel@users.noreply.github.com>
2022-04-28 17:54:25 +0530

    Create ForgetPassword.h

---
+++ ForgetPassword.h
@@ -0,0 +1,14 @@
+#ifndef __ForgetPassword_h
+#define __ForgetPassword_h
+
+#include<string>
+#include "User_module.h"
+
+
+class ForgetPassword : public User 
+{
+    public:
+        string forget();
+};
+
+#endif
